0ee621a54fcdbd0a3f65bf2da3c64c4c285bf077
Sharath Chandra <sharath.chandra@opsmx.io>
2023-01-30 18:00:10 +0530

    feat: added icon for warning in popup

--- ui/src/app/applications/components/utils.tsx
+++ ui/src/app/applications/components/utils.tsx
@@ -123,7 +123,7 @@ export async function deleteApplication(appName: string, appNamespace: string, a
                 }
             }
         },
-        {name: '', color: 'info'},
+        {name: 'argo-icon-warning', color: 'info'},
         '',
         {propagationPolicy: 'foreground'}
     );
