3fce8ab1cd59297a5945e5160dcc2ee4f42ecfd2
NagendraOpsmx <80510611+NagendraOpsmx@users.noreply.github.com>
2024-01-24 16:17:16 +0530

    Argo App creation via Isd changes (#14)

--- ui/src/app/shared/components/layout/layout.tsx
+++ ui/src/app/shared/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Sidebar} from '../../../sidebar/sidebar';
 import {ViewPreferences} from '../../services';
+import { Context } from '../../context';
 
 require('./layout.scss');
 
@@ -12,11 +13,24 @@ export interface LayoutProps {
     isExtension?: boolean;
 }
 
-export const Layout = (props: LayoutProps) => (
+const checkUrlIncludesOpsmx = (param: string) => {
+    let urlSplit = param?.split('/')
+    if(urlSplit && urlSplit[urlSplit.length-2] == 'opsmx' && urlSplit[urlSplit.length-1] == 'creation'){
+        return true;
+    }
+        return false;
+}
+
+export const Layout = (props: LayoutProps) => {
+    const context = React.useContext(Context);
+    const locationPath = context.history.location.pathname;
+    const pathHasOpsmx = checkUrlIncludesOpsmx(locationPath);
+    return (
     <div className={props.pref.theme ? 'theme-' + props.pref.theme : 'theme-light'}>
-        <div className={`cd-layout ${props.isExtension ? 'cd-layout--extension' : ''}`}>
+        <div className={`cd-layout ${props.isExtension ? 'cd-layout--extension' : ''} ${pathHasOpsmx ? 'opsmx_hide': ''}`}>
             <Sidebar onVersionClick={props.onVersionClick} navItems={props.navItems} pref={props.pref} />
             <div className={`cd-layout__content ${props.pref.hideSidebar ? 'cd-layout__content--sb-collapsed' : 'cd-layout__content--sb-expanded'}`}>{props.children}</div>
         </div>
     </div>
 );
+}
